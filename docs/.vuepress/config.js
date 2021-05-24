1c01c83a4a5607000de2e559534baa2401ebb488
褚鹏飞 <540788769@qq.com>
2021-05-24 22:13:32 +0800

    add: feat: 个人网站的搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  title: '褚鹏飞的博客',
-  description: '这是我存在的印记'
+  title: "褚鹏飞的博客",
+  description: "终于等到你",
+
+  themeConfig: {
+    // navbar: false, // 配置这个选项，将会在所有页面禁用导航栏
+    lastUpdated: '最后更新时间',
+    sidebar: 'auto',
+    head: [["link", { rel: "icon", href: "/icon.jpeg" }]],
+    nav: [
+      {
+        text: "前端面试之道",
+        link: '/interview/'
+      },
+      {
+        text: "写作",
+        link: '/writing/'
+      },
+    ],
+    sidebar: [
+    ],
+  },
 }
