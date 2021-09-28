36b20669ef8db34ea188de26cfe479f9209649d5
褚鹏飞 <540788769@qq.com>
2021-09-28 12:39:01 +0800

    feat: 发布服务更新

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,9 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:MagicalBridge/MagicalBridge.github.io.git master
 
+# 推送到自己的服务器指定目录
+scp -r ./docs/.vuepress/dist root@47.103.72.18:/usr/share/nginx/html/vuepress
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
