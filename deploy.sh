d79589a4c21a3312fb1678a935a75e0b68c6d567
褚鹏飞 <540788769@qq.com>
2021-09-28 18:02:25 +0800

    feat: 添加连续子数组的最大和

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ set -e
 # 生成静态文件
 npm run docs:build
 
+# 推送到自己的服务器指定目录
+scp -r ./docs/.vuepress/dist root@47.103.72.18:/usr/share/nginx/html/vuepress
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
@@ -19,8 +22,6 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 git push -f git@github.com:MagicalBridge/MagicalBridge.github.io.git master
 
-# 推送到自己的服务器指定目录
-scp -r ./docs/.vuepress/dist root@47.103.72.18:/usr/share/nginx/html/vuepress
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
